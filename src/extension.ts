f3eeefd6e8398d9f4ed2bc23919d72eb85cdce3e
Irving Juárez <irvingjuarez274@gmail.com>
2022-01-12 16:19:58 -0600

    The code to save data is already added

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
+import * as vscode from 'vscode';
 import { commands, ExtensionContext, window, workspace } from 'vscode';
 import { showInformation } from './services/ShowInformation';
+import { LocalStorage } from "./services/data/LocalStorage";
 
 // this method is called when your extension is activated
 export function activate(context: ExtensionContext): void {
@@ -12,6 +14,26 @@ export function activate(context: ExtensionContext): void {
   );
 
   context.subscriptions.push(cmd);
+
+  // const activeEditor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  // const regex = new RegExp(/\.[a-z]+$/i)
+  // let answer: string;
+  // if(!activeEditor){
+  // 	answer = "No active editor"
+  // }else{
+    // answer = String(activeEditor.document.uri)
+  // }
+
+  // let uri = {
+  // 	value: answer
+  // }
+  // let storageManager = new LocalStorage(context.globalState);
+  // storageManager.setValue("URI", uri)
+
+  // const readUri = storageManager.getValue("URI")
+  // console.log(readUri)
+  // vscode.window.showInformationMessage(answer)
+  // vscode.workspace.onDidChangeWorkspaceFolders(() => doSomething(readUri))
 }
 
 // this method is called when your extension is deactivated
