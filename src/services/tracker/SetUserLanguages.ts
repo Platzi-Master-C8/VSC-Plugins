14d08860decbdcf9a0608324cf94d4b0ffcb3e38
Irving Juárez <irvingjuarez274@gmail.com>
2022-01-17 11:26:21 -0600

    The day when several lenguages were used is being tracked right now

--- src/services/tracker/SetUserLanguages.ts
+++ src/services/tracker/SetUserLanguages.ts
@@ -3,10 +3,6 @@ import { LocalStorage } from "../data/LocalStorage";
 
 const setUserLanguages = (storage: Memento, flag: boolean = false) => {
   const storageManager = new LocalStorage(storage);
-  if(flag){
-    // Here there must be internet comprobation and send/delete/save data
-    storageManager.setValue("GHUserLanguages", null)
-  }
   const activeEditor: TextEditor | undefined = window.activeTextEditor;
   const regex = new RegExp(/\.[a-z]+$/i)
   let answer: string | RegExpMatchArray | "" | null;
@@ -16,25 +12,42 @@ const setUserLanguages = (storage: Memento, flag: boolean = false) => {
   let userLan: any = storageManager.getValue("GHUserLanguages")
   const extension: string | null = answer ? answer[0] : null
 
-  if(userLan && extension){
-    let flag = userLan.value.find((item: any) => item.lenguageExtension === extension)
-    if(!flag){
-      userLan.value.push({
-        lenguageExtension: extension,
-        date: new Date()
-      })
+  if(flag){
+    // Here there must be internet comprobation and send/delete/save data
+    storageManager.setValue("GHUserLanguages", {
+      value: {
+        date: new Date(),
+        lenguages: [extension]
+      }
+    })
+  }else{
+    let doesExtExists = userLan.value.lenguages.find((item: string) => item === extension)
+    if(!doesExtExists){
+      userLan.value.lenguages.push(extension)
       storageManager.setValue("GHUserLanguages", userLan)
     }
-  }else{
-    storageManager.setValue("GHUserLanguages", {
-      value: [
-        {
-          lenguageExtension: extension,
-          date: new Date()
-        }
-      ]
-    })
   }
+
+
+  // if(userLan && extension){
+  //   let flag = userLan.value.lenguages.find((item: string) => item === extension)
+  //   if(!flag){
+  //     userLan.value.lenguages.push({
+  //       lenguageExtension: extension,
+  //       date: new Date()
+  //     })
+  //     storageManager.setValue("GHUserLanguages", userLan)
+  //   }
+  // }else{
+  //   storageManager.setValue("GHUserLanguages", {
+  //     value: [
+  //       {
+  //         lenguageExtension: extension,
+  //         date: new Date()
+  //       }
+  //     ]
+  //   })
+  // }
 
   console.log(storageManager.getValue("GHUserLanguages"))
 };
