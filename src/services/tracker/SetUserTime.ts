f7f0d39a25dafde3783b4daffef46fc496e3f26e
Irving Juárez <irvingjuarez274@gmail.com>
2022-01-24 15:01:16 -0600

    Everytime the user types, an action is triggered

--- src/services/tracker/SetUserTime.ts
+++ src/services/tracker/SetUserTime.ts
@@ -1,3 +1,14 @@
-export const setUserTime = () => {
-  // TODO: save time tracked while user open their files
+import { TextEditor, window, workspace } from "vscode";
+
+const setUserTime = () => {
+  const currentTextEditor: TextEditor | undefined = window.activeTextEditor
+  if(currentTextEditor){
+    workspace.onDidChangeTextDocument((e) => {
+      if(e.contentChanges.length){
+        console.log("onDidChangeTextDocument:", e)
+      }
+    })
+  }
 };
+
+export { setUserTime }
